2f8e2fe7b17b9126be51b490238af99e5523b4ff
Swap <swap@swap.com>
2021-07-13 17:05:32 +0100

    add js folder and file

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// make the showcase element show with a amazing animation
+
+const showcaseText = document.querySelector('.showcase-text')
+const showcaseForm = document.querySelector('.showcase-form')
+
+window.addEventListener("DOMContentLoaded", function() {
+    showcaseText.classList.add('showcase-left')
+    showcaseForm.classList.add('showcase-right')
+})
